f1fc55b91d48cfcd71623e35e98e801a33a29a24
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-08-16 19:23:54 +0000

    Remove default finetune behavior and update scripts

--- ner.py
+++ ner.py
@@ -6,8 +6,10 @@ import numpy as np
 import random
 import torch
 import argparse
+from pathlib import Path
+from flair.training_utils import EvaluationMetric
 
-def train_ner(embedding, resultdir, datadir='resources/tasks', use_crf=False, lr=0.1):
+def train_ner(embedding, resultdir, datadir='resources/tasks', use_crf=False, lr=0.1, finetune=False):
     # 1. get the corpus
     corpus: Corpus = NLPTaskDataFetcher.load_corpus(NLPTask.CONLL_03, base_path=datadir)
     print(corpus)
@@ -40,7 +42,8 @@ def train_ner(embedding, resultdir, datadir='resources/tasks', use_crf=False, lr
                                             embeddings=embeddings,
                                             tag_dictionary=tag_dictionary,
                                             tag_type=tag_type,
-                                            use_crf=use_crf)
+                                            use_crf=use_crf,
+                                            relearn_embeddings=finetune)
 
     # 6. initialize trainer
     from flair.trainers import ModelTrainer
@@ -54,13 +57,57 @@ def train_ner(embedding, resultdir, datadir='resources/tasks', use_crf=False, lr
                 max_epochs=150,
                 monitor_test=True)
 
+def eval_ner(embedding, resultdir, datadir='resources/tasks', use_crf=False):
+    # 1. get the corpus
+    corpus: Corpus = NLPTaskDataFetcher.load_corpus(NLPTask.CONLL_03, base_path=datadir)
+    print(corpus)
+
+    # 2. what tag do we want to predict?
+    tag_type = 'ner'
+
+    # 3. make the tag dictionary from the corpus
+    tag_dictionary = corpus.make_tag_dictionary(tag_type=tag_type)
+    print(tag_dictionary.idx2item)
+
+    # 4. initialize embeddings
+    embedding_types: List[TokenEmbeddings] = [
+
+        WordEmbeddings(embedding),
+    ]
+
+    embeddings: StackedEmbeddings = StackedEmbeddings(embeddings=embedding_types)
+
+    # 5. initialize sequence tagger
+    from flair.models import SequenceTagger
+
+    tagger: SequenceTagger = SequenceTagger(hidden_size=256,
+                                            embeddings=embeddings,
+                                            tag_dictionary=tag_dictionary,
+                                            tag_type=tag_type,
+                                            use_crf=use_crf,
+                                            relearn_embeddings=False)
+
+    # load checkpoitns
+    checkpoint = tagger.load_checkpoint(f'{resultdir}/best-model.pt')
+
+    # 6. initialize trainer
+    from flair.trainers import ModelTrainer
+
+    trainer = ModelTrainer.load_from_checkpoint(checkpoint, corpus)
+    trainer.final_test(Path(resultdir),
+        embeddings_in_memory=True,
+        evaluation_metric=EvaluationMetric.MICRO_F1_SCORE,
+        eval_mini_batch_size=32)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="")
     parser.add_argument("--embedding", type=str, required=True)
     parser.add_argument("--resultdir", type=str, required=True)
     parser.add_argument("--seed", type=int, required=True)
     parser.add_argument("--lr", type=float, required=True)
-    parser.add_argument("--use_crf", action='store_true', required=True)
+    parser.add_argument("--use_crf", type=bool, default=False)
+    parser.add_argument("--finetune", type=bool, default=False)
+    parser.add_argument("--eval", action='store_true')
     args = parser.parse_args()
     seed = args.seed
     embedding = args.embedding
@@ -72,4 +119,7 @@ if __name__ == "__main__":
         torch.backends.cudnn.deterministic=True
     np.random.seed(seed)
     random.seed(seed)
-    train_ner(embedding, resultdir, use_crf=args.crf, lr=args.lr)
+    if not args.eval:
+        train_ner(embedding, resultdir, use_crf=args.use_crf, lr=args.lr, finetune=finetune)
+    else:
+        eval_ner(embedding, resultdir, use_crf=args.use_crf)
