24c1957b3aeb9cafdbcbcd1189891b750eec708f
Avner May <avnermay@cs.stanford.edu>
2019-10-17 22:23:59 -0700

    Fixing logging issue

--- flair/__init__.py
+++ flair/__init__.py
@@ -21,24 +21,24 @@ import logging.config
 
 __version__ = "0.4.2"
 
-logging.config.dictConfig(
-    {
-        "version": 1,
-        "disable_existing_loggers": False,
-        "formatters": {"standard": {"format": "%(asctime)-15s %(message)s"}},
-        "handlers": {
-            "console": {
-                "level": "INFO",
-                "class": "logging.StreamHandler",
-                "formatter": "standard",
-                "stream": "ext://sys.stdout",
-            }
-        },
-        "loggers": {
-            "flair": {"handlers": ["console"], "level": "INFO", "propagate": False}
-        },
-        "root": {"handlers": ["console"], "level": "WARNING"},
-    }
-)
+# logging.config.dictConfig(
+#     {
+#         "version": 1,
+#         "disable_existing_loggers": False,
+#         "formatters": {"standard": {"format": "%(asctime)-15s %(message)s"}},
+#         "handlers": {
+#             "console": {
+#                 "level": "INFO",
+#                 "class": "logging.StreamHandler",
+#                 "formatter": "standard",
+#                 "stream": "ext://sys.stdout",
+#             }
+#         },
+#         "loggers": {
+#             "flair": {"handlers": ["console"], "level": "INFO", "propagate": False}
+#         },
+#         "root": {"handlers": ["console"], "level": "WARNING"},
+#     }
+# )
 
 logger = logging.getLogger("flair")
